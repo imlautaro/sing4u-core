1ad01828e6cc9cfba9828578a2b20646fa241f63
= <hi@imlautaro.com>
2023-01-02 14:40:18 -0300

    add google-fonts module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ export default defineNuxtConfig({
 			size: '1.5em',
 		},
 	},
-	modules: ['nuxt-icon', 'nuxt-windicss'],
+	googleFonts: {
+		download: true,
+		families: {
+			Nunito: [600, 700, 800],
+		},
+	},
+	modules: ['nuxt-icon', 'nuxt-windicss', '@nuxtjs/google-fonts'],
 	srcDir: 'src',
 })
