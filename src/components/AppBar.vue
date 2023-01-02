ffde0d0220ea2bdfa321855054c412a9948117aa
= <hi@imlautaro.com>
2023-01-02 15:13:13 -0300

    add app-bar component

---
+++ src/components/AppBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ elevate?: boolean }>()
+</script>
+
+<template>
+	<nav
+		class="bg-surface border-default duration-100 p-4"
+		:class="{ 'border-b': elevate }"
+	>
+		<Stack gap="4" items="center" justify="between">
+			<slot />
+		</Stack>
+	</nav>
+</template>
