64f0c682aba8942d6e7fd383dd8011b415f28b7d
= <hi@imlautaro.com>
2023-01-02 15:33:51 -0300

    add list-item-link component

---
+++ src/components/List/Item/Link.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{ icon?: string; endIcon?: string }>()
+</script>
+
+<template>
+	<NuxtLink
+		class="duration-100 btn-ghost flex items-center px-5 py-4 sm:(px-4 py-3) rounded-2xl space-x-4 w-full"
+	>
+		<slot name="icon" />
+		<Icon v-if="icon" class="text-secondary" :name="icon" />
+		<span class="flex-1 w-full">
+			<slot />
+		</span>
+		<slot name="end-icon" />
+		<Icon v-if="endIcon" class="text-secondary" :name="endIcon" />
+	</NuxtLink>
+</template>
