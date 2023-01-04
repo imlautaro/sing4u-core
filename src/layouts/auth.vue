820dd7d81c79155c7c6734233cf160c2f3048650
Lautaro Pereyra <hi@imlautaro.com>
2023-01-04 19:02:01 -0300

    add auth layout

---
+++ src/layouts/auth.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="grid grid-cols-6 lg:grid-cols-12 lg:grid-rows-12 min-h-screen">
+		<div class="fixed left-4 top-4">
+			<button
+				@click="navigateTo('/')"
+				class="duration-100 flex p-2 rounded-full text-current nt:(hover:bg-black/5 !md:hover:bg-white/10) !active:bg-black/10 md:(text-white !active:bg-white/25)"
+			>
+				<Icon name="lucide:arrow-left" />
+			</button>
+		</div>
+		<div
+			class="hidden md:flex bg-primary-600 dark:bg-neutral-800 col-span-12 col-start-1 row-span-5 row-start-1"
+		/>
+		<div
+			class="hidden md:flex bg-slate-100 dark:bg-neutral-900 col-span-12 col-start-1 row-span-7 row-start-6"
+		/>
+		<div
+			class="col-span-6 md:(col-span-4 col-start-2) lg:(col-span-6 col-start-4) xl:(col-span-4 col-start-5) row-span-12 row-start-1 flex flex-col justify-center"
+		>
+			<Stack
+				class="bg-surface border-default duration-200 h-full p-6 md:(p-12 rounded-3xl shadow-2xl) md:(dark:border h-min)"
+				justify="center"
+				vertical
+			>
+				<slot />
+			</Stack>
+		</div>
+	</div>
+</template>
