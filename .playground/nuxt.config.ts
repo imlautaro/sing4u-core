551a0df8573346b8c76884f57f9c8b00b10a9051
Lautaro Pereyra <hi@imlautaro.com>
2023-01-04 19:04:39 -0300

    create basic structure for playground

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -1,6 +1,19 @@
+import colors from 'windicss/colors'
+
 export default defineNuxtConfig({
 	extends: '..',
 	typescript: {
 		includeWorkspace: true,
+	},
+	windicss: {
+		config: {
+			theme: {
+				extend: {
+					colors: {
+						primary: colors.teal,
+					},
+				},
+			},
+		},
 	},
 })
---
+++ .playground/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+
+const { logout } = useAuth()
+</script>
+
+<template>
+	<div>
+		<AppBar elevate>
+			<Brand />
+			<ClientOnly>
+				<Button v-if="user" @click="logout" icon="lucide:log-out">
+					Cerrar sesión
+				</Button>
+				<Button v-else color="primary" icon="lucide:log-in" to="/login">
+					Iniciar sesión
+				</Button>
+			</ClientOnly>
+		</AppBar>
+		<h1>Home Page</h1>
+	</div>
+</template>
