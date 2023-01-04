00e62ba721e54c74386938782a0ed3aff61c9bb0
Lautaro Pereyra <hi@imlautaro.com>
2023-01-04 19:03:10 -0300

    add login page

---
+++ src/pages/login.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: 'auth',
+	middleware: 'unauthenticated',
+})
+
+const { email, errors, login, loginWith, pending } = useAuth()
+</script>
+
+<template>
+	<Stack gap="6" vertical>
+		<Stack v-if="$route.query.sent" gap="4" vertical>
+			<Stack
+				class="border border mx-auto p-6 rounded-full text-3xl text-success-600"
+			>
+				<Icon name="lucide:mail-check" />
+			</Stack>
+			<Stack class="text-center" gap="2" vertical>
+				<h1 class="font-bold text-2xl">
+					Te enviamos un enlace a tu correo para ingresar
+				</h1>
+				<p class="text-secondary">
+					Si no lo ves, por favor revisa el apartado de spam
+				</p>
+			</Stack>
+		</Stack>
+		<template v-else>
+			<Stack gap="4" vertical>
+				<Stack
+					class="dark:bg-white/5 border mx-auto p-6 rounded-full text-3xl"
+				>
+					<Icon name="lucide:user" />
+				</Stack>
+				<h1 class="font-bold text-2xl text-center">
+					Ingresar a <b>Sing4U</b>
+				</h1>
+			</Stack>
+			<hr class="invisible" />
+			<Button
+				@click="loginWith('google')"
+				icon="logos:google-icon"
+				icon-left
+			>
+				Continuar con Google
+			</Button>
+			<span class="text-center text-secondary">ó</span>
+			<Alert v-if="errors.general" type="error">
+				{{ errors.general }}
+			</Alert>
+			<Stack component="form" gap="6" @submit.prevent="login" vertical>
+				<TextField
+					v-model="email"
+					icon="lucide:mail"
+					label="Correo electrónico"
+					type="email"
+					:error="errors.email"
+				/>
+				<div class="flex flex-col space-y-2">
+					<Button
+						:pending="pending"
+						color="primary"
+						icon="lucide:log-in"
+						type="submit"
+					>
+						Continuar con Email
+					</Button>
+				</div>
+				<hr class="invisible" />
+				<small
+					class="text-center leading-normal text-sm text-secondary"
+				>
+					Al continuar aceptas nuestros
+					<NuxtLink class="text-link" to="/terms-and-conditions">
+						términos y condiciones
+					</NuxtLink>
+					y nuestras
+					<NuxtLink class="text-link" to="/privacy-policy"
+						>políticas de privacidad</NuxtLink
+					>.
+				</small>
+			</Stack>
+		</template>
+	</Stack>
+</template>
