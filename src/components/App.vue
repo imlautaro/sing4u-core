8afc81f21f8925e3c703c6ec95c73eafea0e2761
Lautaro Pereyra <hi@imlautaro.com>
2023-01-07 12:58:40 -0300

    add app wrapper component

---
+++ src/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div
+		class="bg-white lg:(bg-slate-100 dark:bg-neutral-900) dark:bg-neutral-800 h-screen overflow-hidden text-body lg:text-lg"
+	>
+		<Stack vertical>
+			<NuxtLayout>
+				<NuxtPage />
+			</NuxtLayout>
+			<slot />
+		</Stack>
+		<Backdrop />
+		<UpdatePrompt />
+	</div>
+</template>
