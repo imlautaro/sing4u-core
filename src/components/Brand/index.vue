360e72c3236d55c815bbc563a27b8a4fdd4d8423
Lautaro Pereyra <hi@imlautaro.com>
2023-01-08 19:19:40 -0300

    add link to home page in brand component

--- src/components/Brand/index.vue
+++ src/components/Brand/index.vue
@@ -1,8 +1,8 @@
 <template>
-	<Stack items="center" gap="4">
+	<NuxtLink class="flex items-center space-x-4" to="/">
 		<Stack class="bg-primary-600 border-dashed p-2 text-white rounded-2xl">
 			<BrandIsotype class="h-8 w-8" />
 		</Stack>
 		<BrandLogotype />
-	</Stack>
+	</NuxtLink>
 </template>
