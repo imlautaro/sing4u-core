301ec33ee473c37f06078292c43cef7ace907d6d
Lautaro Pereyra <hi@imlautaro.com>
2023-01-09 21:14:37 -0300

    add active status to list item link component

--- src/components/List/Item/Link.vue
+++ src/components/List/Item/Link.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-defineProps<{ icon?: string; endIcon?: string }>()
+defineProps<{ icon?: string; endIcon?: string; active?: boolean }>()
 </script>
 
 <template>
 	<NuxtLink
-		class="duration-100 btn-ghost flex items-center px-5 py-4 sm:(px-4 py-3) rounded-2xl space-x-4 w-full"
+		class="duration-100 flex items-center px-5 py-4 sm:(px-4 py-3) rounded-2xl space-x-4 w-full"
+		:class="
+			active
+				? 'bg-primary-600/10 dark:bg-primary-400/10 text-primary'
+				: 'btn-ghost'
+		"
 	>
 		<slot name="icon" />
-		<Icon v-if="icon" class="text-secondary" :name="icon" />
+		<Icon
+			v-if="icon"
+			:class="active ? 'text-primary' : 'text-secondary'"
+			:name="icon"
+		/>
 		<span class="flex-1 w-full">
 			<slot />
 		</span>
